077144d19af103303516256cb6927ebbd0bbf4cc
RedLine Souls Server <server@redlinesouls.local>
2025-10-14 11:06:18 +0000

    Simple welcome audio: use WelcomeMessageAudioUrl

--- csp_scripts/welcome_sound.lua
+++ csp_scripts/welcome_sound.lua
@@ -1,30 +1,15 @@
--- Welcome sound script for RedLine Souls
--- Plays intro sound 5 seconds after spawning in pits
-
+-- Welcome sound for RedLine Souls
 local played = false
-local spawnTime = 0
-local inPits = false
+local timer = 0
 
 function script.update(dt)
-    -- Check if player is in pits
-    local currentInPits = ac.isInPits()
+    if played then return end
     
-    -- Detect spawn/join (first time in pits)
-    if currentInPits and not inPits then
-        spawnTime = os.clock()
-    end
+    timer = timer + dt
     
-    inPits = currentInPits
-    
-    -- Play sound 5 seconds after spawn, only once per session
-    if not played and inPits and spawnTime > 0 then
-        local timeSinceSpawn = os.clock() - spawnTime
-        
-        if timeSinceSpawn >= 5 then
-            -- Play the welcome sound
-            ac.loadSoundEvent('content/sfx/RedLine Souls Intro.m4a'):play()
-            played = true
-            ac.log('RedLine Souls welcome sound played!')
-        end
+    -- Play after 5 seconds
+    if timer >= 5 then
+        ui.playSound('/content/sfx/RedLine Souls Intro.m4a')
+        played = true
     end
 end
